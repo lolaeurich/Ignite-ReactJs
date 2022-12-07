3309c37a630d6aa50f5448487ed4a6aac2a9108e
lolaeurich <lola.tatti@gmail.com>
2022-12-07 16:59:20 -0300

    feat: all colors root made

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react' //é o pacote responsável pelo core do React, as funcionalidades básicas.
+import ReactDOM from 'react-dom/client' //integração do core do React com o DOM (Document Object Model), a representação do nosso html através do javascript
+import { App } from "./App"
 
+
+//o elemento raiz da nossa html é o root
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
